cb250335d782fa52db3fcea9febf2aa142c53386
miokato <miokato@me.com>
2021-01-30 21:23:57 +0900

    Comment out assert. https://github.com/rsms/peertalk/pull/44

--- peertalk/PTUSBHub.m
+++ peertalk/PTUSBHub.m
@@ -488,8 +488,8 @@ static NSString *kPlistPacketTypeConnect = @"Connect";
     char *buffer = NULL;
     size_t buffer_size = 0;
     PT_PRECISE_LIFETIME_UNUSED dispatch_data_t map_data = dispatch_data_create_map(data, (const void **)&buffer, &buffer_size); // objc_precise_lifetime guarantees 'map_data' isn't released before memcpy has a chance to do its thing
-    assert(buffer_size == sizeof(ref_upacket.size));
-    assert(sizeof(upacket_len) == sizeof(ref_upacket.size));
+//    assert(buffer_size == sizeof(ref_upacket.size));
+//    assert(sizeof(upacket_len) == sizeof(ref_upacket.size));
     memcpy((void *)&(upacket_len), (const void *)buffer, buffer_size);
 
     // Allocate a new usbmux_packet_t for the expected size
